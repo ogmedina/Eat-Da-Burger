7a42ac3d91d9668cfe9299d0caf3819130b905a4
Oscar Medina <ogmedina04@gmail.com>
2021-01-21 12:50:32 -0600

    added partial for list with buttons and states, added ajax calls to change status of the burger

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,4 +1,26 @@
 $(function() {
+    //function to eat burger and change the state of devoured
+    $(".devour-burger").on("click", function(event){
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devoured: newDevour
+        };
+
+        //Send the new PUT request for Burger Eating
+        $.ajax("/api/burgers/" + id,{
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function(){
+                console.log("changed devoured state to", newDevour);
+                //Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+    //function to add burger to db
     $("#add-burger").on("submit", function(event){
         event.preventDefault();
         console.log(event);
@@ -7,13 +29,14 @@ $(function() {
             devoured: 0
         };
         console.log(newBurger);
-    
+        //Send the new POST request for the new burger
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
             function() {
                 console.log("added new burger");
+                //Reload the page to get the updated list
                 location.reload();
             }
         );
