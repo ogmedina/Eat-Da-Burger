169465d8b85df38d5983d22b10afcd2eb521e81f
Oscar Medina <ogmedina04@gmail.com>
2021-01-21 09:55:47 -0600

    added basic HTML look, need to add ajax calls in .js and continue to fix front-end for final product

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const express = require("express");
+
+//Port for Heroku Deployment
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Static content for the app from the "public" directory
+app.use(express.static("public"));
+
+//Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
